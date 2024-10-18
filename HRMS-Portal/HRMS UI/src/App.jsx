bbadc58221e7c795d1b27dc4e826ed397c49e162
Akash N N <akashnn2003@gmail.com>
2024-10-18 15:52:24 +0530

    commiting the tailwind module

--- HRMS-Portal/HRMS UI/src/App.jsx
+++ HRMS-Portal/HRMS UI/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Employee from './components/Employee';
+import Categorie from './components/Categorie';
+import Profile from './components/Profile';
+import Counter from './components/Counter'
+import Fetch from './components/Fetch';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    
+
+    <BrowserRouter>
+    
+      
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/employee" element={<Employee />} />
+        <Route path="/categorie" element={<Categorie/>}/>
+        <Route path="/logout" element={<Login/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
